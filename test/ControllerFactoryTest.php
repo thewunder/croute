2ece4738808d0c34e98d080bbb9f757a44c88893
Michael O'Connell <moconnell@lifestylepubs.com>
2014-09-26 11:55:52 -0500

    add ControllerFactory test

---
+++ test/ControllerFactoryTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Croute;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ControllerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndexController()
+    {
+        $factory = $this->getFactory();
+
+        $controller = $factory->getController(Request::create('/'));
+        $this->assertTrue($controller instanceof IndexController);
+    }
+
+    public function testNamedController()
+    {
+        $factory = $this->getFactory();
+
+        $controller = $factory->getController(Request::create('/named/'));
+        $this->assertTrue($controller instanceof NamedController);
+    }
+
+    public function testSanitization()
+    {
+        $factory = $this->getFactory();
+
+        $controller = $factory->getController(Request::create('/nam..\..ed/'));
+        $this->assertTrue($controller instanceof NamedController);
+    }
+
+    public function testNamespacedControllers()
+    {
+        $factory = $this->getFactory();
+
+        $controller = $factory->getController(Request::create('/myNamespace/'));
+        $this->assertTrue($controller instanceof \Croute\MyNamespace\IndexController);
+
+        $controller = $factory->getController(Request::create('/myNamespace/named/'));
+        $this->assertTrue($controller instanceof \Croute\MyNamespace\NamedController);
+    }
+
+    protected function getFactory()
+    {
+        return new ControllerFactory(['Croute'], []);
+    }
+}
+
+class IndexController extends Controller
+{
+}
+
+class NamedController extends Controller
+{
+}
+
+namespace Croute\MyNamespace;
+
+use Croute\Controller;
+
+class IndexController extends Controller
+{
+}
+
+class NamedController extends Controller
+{
+}
